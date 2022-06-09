babfc1d67790ef07f8ea421f9f644a88a9141f19
Pinkal Mistry <pinkal@cafu.com>
2022-06-09 10:40:31 +0400

    Coin list view model created with use case integrated

---
+++ app/src/main/java/com/pm/mvvmcryptocurrencyapp/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,45 @@
+package com.pm.mvvmcryptocurrencyapp.presentation.coin_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pm.mvvmcryptocurrencyapp.common.Resource
+import com.pm.mvvmcryptocurrencyapp.domain.use_case.get_coins.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val coinListUseCase: GetCoinsUseCase,
+) : ViewModel() {
+
+    private val _state = mutableStateOf<CoinListState>(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        coinListUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(
+                        coins = result.data ?: emptyList()
+                    )
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(
+                        error = result.message ?: "An unexpected error occurred"
+                    )
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
